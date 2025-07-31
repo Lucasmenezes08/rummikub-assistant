2e88bc0f9695834c2a737d435d91fe59b3dc14ad
Lucasmenezes08 <lms4@cesar.school>
2025-07-31 16:21:43 -0300

    :construction: Estruturar componente PlayerInput.

--- rummikub/src/routers/playerName.jsx
+++ rummikub/src/routers/playerName.jsx
@@ -1,5 +1,41 @@
+import PlayerInput from "../components/playerName/playerInput";
+import { useNavigate } from "react-router-dom";
+import useBackNavigation from "../hooks/useBackNavigation";
+import { useContext } from "react";
+import { ThemeContext } from "../store/darkThemeContext";
+import { PlayerQuantidadeContext } from "../store/playerQuantidadeContext";
+import BackButton from "../components/common/button-back";
+
+
 export default function PlayerName(){
+    const navigate = useNavigate();
+    const backRouter = useBackNavigation();
+    const {tema} = useContext(ThemeContext);
+    const {quantidade , alterarQuantidade} = useContext(PlayerQuantidadeContext);
+    
+    function handleConfirm (){
+        if (!quantidade){
+            return
+        }
+        navigate('/playername');
+    }
+
+
     return (
-        <h1>Nome do player</h1>
+         <section className={`relative h-screen w-full ${tema === 'dark' ? 'bg-slate-900 text-white' : 'bg-white text-black'}`}>
+            
+            <section className="absolute top-[2%] -translate-y-[2%] px-2">
+                <BackButton onClick={backRouter}/>
+            </section>
+
+            <section className="absolute top-[2%] -translate-y-[2%] left-[50%] -translate-x-[50%]">
+                <section className="flex flex-col items-center">
+                    <h2 className="font-semibold text-center text-xl mb-[4rem]">Nome dos jogadores</h2>
+
+                    <PlayerInput/>
+                </section>
+            </section>
+
+        </section>
     )
 }

42771aee47df11c166abf1b7e0011f2f175c1e41
Lucasmenezes08 <lms4@cesar.school>
2025-08-01 10:00:15 -0300

    :construction: Adicionar estado global para manipulação de rodadas.

--- rummikub/src/store/playerInfo.jsx
+++ rummikub/src/store/playerInfo.jsx
@@ -9,19 +9,26 @@ export default function PlayerProvider ({children}){
         const playerObject = sessionStorage.getItem('playerInfo');
         return playerObject ? JSON.parse(playerObject) : [];
     })
+    const [rodada , setRodada] = useState(() => {
+        const rodadaAtual = sessionStorage.getItem('rodada');
+        return rodadaAtual ? JSON.parse(rodadaAtual) : null;
+    });
+
 
     function setPlayers (array) {
         const newArray = array.map((name , index) =>({
             id: index,
             name: name,
-            score: 0,
+            scoreRodada: 0,
+            scoreTotal: 0,
         }))
         setPlayerInfo(newArray)
     }
 
     useEffect (() => {
-        sessionStorage.setItem('playerInfo', JSON.stringify(playerInfo))
-    }, [playerInfo]);
+        sessionStorage.setItem('playerInfo', JSON.stringify(playerInfo));
+        sessionStorage.setItem('rodada', JSON.stringify(rodada));
+    }, [playerInfo , rodada]);
 
     const value = {
         players: playerInfo,

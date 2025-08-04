1455afc5bfa938f8429e71c2cf53bad8a7c44540
Lucasmenezes08 <lms4@cesar.school>
2025-08-04 11:46:41 -0300

    :sparkles: feat: Implementar função reiniciar jogo e integrar rota inicial.

--- rummikub/src/routers/resultado.jsx
+++ rummikub/src/routers/resultado.jsx
@@ -3,10 +3,20 @@ import { ThemeContext } from "../store/darkThemeContext"
 import { PlayerContext } from "../store/playerInfo";
 import { ResultadoGrid } from "../components/resultado/resultado-grid";
 import ConfirmButton from "../components/common/button-confirm";
+import { useNavigate } from "react-router-dom";
+
 
 export default function Resultado (){
+    const navigate = useNavigate();
     const {tema} = useContext(ThemeContext);
-    const {setPlayers , setRound , setRodada, rodadas , players , UpdateRoundScore , finishRound} = useContext(PlayerContext);
+    const {setPlayers , resetGame, setRound , setRodada, rodadas , players , UpdateRoundScore , finishRound} = useContext(PlayerContext);
+
+
+
+    function handleRestartGame (){
+        navigate('/');
+        resetGame();
+    }
 
     return (
         <section className={`flex flex-col min-h-screen w-full overflow-y-hidden ${tema === 'dark' ? 'bg-slate-900 text-white' : 'bg-white text-black'}`}>
@@ -21,7 +31,7 @@ export default function Resultado (){
                     </section>
 
                     <section className="pb-5">
-                        <ConfirmButton mensagem={'Reiniciar jogo'}/>
+                        <ConfirmButton mensagem={'Reiniciar jogo'} onClick={handleRestartGame}/>
                     </section>
                 </section>
 

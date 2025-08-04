ed6692c1f4f7be44173c50b4532804b3118475e6
Lucasmenezes08 <lms4@cesar.school>
2025-08-04 15:09:38 -0300

    :dizzy: style: Animar componente homepage.

--- rummikub/src/routers/homepage.jsx
+++ rummikub/src/routers/homepage.jsx
@@ -3,7 +3,7 @@ import ConfirmButton from "../components/common/button-confirm";
 import DarkThemeButton from "../components/common/button-darkTheme";
 import { ThemeContext } from "../store/darkThemeContext";
 import { Link, useNavigate } from "react-router-dom";
-
+import { motion } from 'framer-motion';
 
 export default function Homepage (){
     const {tema , alterarTema} = useContext(ThemeContext);
@@ -14,7 +14,12 @@ export default function Homepage (){
         navigate('/quantidade');
     }
     return (
-        <section className={` relative h-screen w-full ${tema === 'dark' ? 'bg-slate-900' : 'bg-white'}`}>
+        <motion.section 
+            className={` relative h-screen w-full ${tema === 'dark' ? 'bg-slate-900' : 'bg-white'}`}
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+        >
             {
                 /*
             <section className="absolute top-[10%] right-[2%] -translate-x-[2%] -translate-y-[90%] ">
@@ -32,7 +37,7 @@ export default function Homepage (){
             <section className="absolute  top-[90%] left-[50%] -translate-x-[50%] -translate-y-[90%]">
                 <ConfirmButton mensagem={'Vamos nessa!'} onClick={handleNavigate}></ConfirmButton>
             </section>
-        </section>
+        </motion.section>
         
     )
 }

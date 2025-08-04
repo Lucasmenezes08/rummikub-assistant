0efed2a80fd0305b99662cc61630e6797fa718f1
Lucasmenezes08 <lms4@cesar.school>
2025-08-04 16:13:04 -0300

    :dizzy: style: Animar componente Resultado.

--- rummikub/src/components/resultado/resultado-grid.jsx
+++ rummikub/src/components/resultado/resultado-grid.jsx
@@ -1,6 +1,22 @@
 import { PlayerContext } from "../../store/playerInfo";
 import { ResultadoPlayer } from "./resultado-info";
 import { useContext } from "react";
+import { motion } from "framer-motion";
+
+
+
+const variantsContainer = {
+    hidden: {opacity: 0},
+    visible: {
+        opacity: 1,
+        transition: {
+            staggerChildren: 1.7
+        }
+    }
+
+}
+
+
 
 export function ResultadoGrid () {
     const { players } = useContext(PlayerContext);
@@ -10,10 +26,10 @@ export function ResultadoGrid () {
     
     
     return (
-        <section>
+        <motion.section variants={variantsContainer} initial='hidden' animate='visible'>
             {rankedPlayers.map((player , index) => 
                 <ResultadoPlayer key={player.id} player={player} rank={index + 1}/>
             )}
-        </section>
+        </motion.section>
     )
 }
